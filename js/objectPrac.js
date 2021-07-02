621ee6900b496130057566591c578cf349591115
warisu <abdullahiabdulwaris@gmail.com>
2021-07-02 15:10:19 +0100

    first commit

---
+++ js/objectPrac.js
@@ -0,0 +1,35 @@
+// // Datatypes
+// // String;
+// // Numbers;
+// // Object {}
+// // Arrays []
+// // Boolean T or F
+// 0, 1, 2, 3, 4;
+// var x = 23;
+// const b = 50;
+// let str = 'John';
+// let str1 = 4 + 5 + 'Peter' * 33 ;
+// let y = x + 23 + b;
+// let addedAndMultiplied = x - 23 * 2 / b;
+// let bool = true;
+// let obj = { name: 'Abdul', age: 25, height: '45FT' }
+// let arr = [25, 'John Knnedy', obj, bool, str1];
+// let myName = obj.age;
+// let pusher = arr.splice(2, 1);
+// let notEql = addedAndMultiplied == str1;
+// let strictEqul = 123 == '123' && y == str;
+
+// console.log(document.getElementsByClassName('bgThistle'));
+// // [0].innerHTML = arr
+// console.log(b, y, str, str1, bool, myName, arr, arr.length, str.length, addedAndMultiplied, notEql, strictEqul);
+
+// // Operators
+// // + -> Addition;
+// // - -> Subtraction;
+// // * -> Multiplicaation;
+// // / -> Division;
+// // != -> Not Equal to;
+// //  == -> Equal to;
+// //  === -> Strict eqaul to;
+// //  || -> OR sign;
+// // && AND sign;
---
+++ js/playingAround.js
@@ -0,0 +1,36 @@
+// Datatypes
+// String;
+// Numbers;
+// Object {}
+// Arrays []
+// Boolean T or F
+0, 1, 2, 3, 4;
+var x = 23;
+const b = 50;
+let str = 'John';
+let str1 = 4 + 5 + 'Peter';
+let y = x + 23 + b;
+let addedAndMultiplied = x - 23 * 2 / b;
+let bool = true;
+let obj = { name: 'Abdul', age: 25, height: '45FT' }
+let arr = [25, 'John Kennedy', obj, bool, str1];
+let myName = obj.age;
+let pusher = arr.unshift();
+let notEql = addedAndMultiplied == str1;
+let strictEqul = 123 == '123' && y == str;
+
+// console.log(document.getElementsByClassName('bgThistle'));
+// [0].innerHTML = arr
+console.log(obj);
+// document.getElementById('para');
+
+// Operators
+// + -> Addition;
+// - -> Subtraction;
+// * -> Multiplicaation;
+// / -> Division;
+// != -> Not Equal to;
+//  == -> Equal to;
+//  === -> Strict eqaul to;
+//  || -> OR sign;
+// && AND sign;
